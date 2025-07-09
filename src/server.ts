6acebd71639f9f19badc67bb126eccf7648f54ae
zlucasftw <ilucasmcpvp@gmail.com>
2025-07-08 23:11:27 -0300

    fix: add forgot cors

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.register(fastify, {
+app.register(fastifyCors, {
     origin: 'http://localhost:5173',
 });
 
